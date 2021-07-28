bea120d0a795b07fe7c611effc96c3d8cc940071
EyVanVen <62552937+EyVanVen@users.noreply.github.com>
2021-07-28 18:06:08 +0300

    Delete src directory

--- src/main/java/com/app/authorize/NotAuthorizedException.java
+++
@@ -1,9 +0,0 @@
-package com.app.authorize;
-
-public class NotAuthorizedException extends RuntimeException {
-
-    public NotAuthorizedException(String message) {
-        super(message);
-    }
-
-}
